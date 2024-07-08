80fd69603482388241c197e2deeda8b792b75329
salman <salmannisthar123@gmail.com>
2024-07-08 18:44:13 +0530

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express()
 const port =3000
 
 app.get("/",(req,res)=>{
-    res.status(200).json({message:'Success. update3..'})
+    res.status(200).json({message:'Success. update1..'})
 })
 
 app.listen(port,()=>{
